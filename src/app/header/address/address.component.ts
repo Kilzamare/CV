49594e425276664419c3d439df5f1f4f9e605b46
Kilzamare <s.kot97@onet.pl>
2019-05-17 11:19:20 +0200

    next step

--- src/app/header/address/address.component.ts
+++ src/app/header/address/address.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-address',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./address.component.sass']
 })
 export class AddressComponent implements OnInit {
+  @Input() title: string;
+  @Input() content: string;
 
   constructor() { }
 
